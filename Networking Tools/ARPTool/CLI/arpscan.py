160f906b746b8ab58a25ec5d48d21f0d295a030b
Alex Martínez <76045216+amtzespinosa@users.noreply.github.com>
2023-05-08 22:53:55 +0200

    Add files via upload

--- Networking Tools/ARPTool/CLI/arpscan.py
+++ Networking Tools/ARPTool/CLI/arpscan.py
@@ -1,8 +1,7 @@
 from scapy.all import ARP, Ether, srp
-import argparse
-from datetime import datetime
+import sys
 
-def arp_scan():
+def arp_scan(target_ip):
 
     arp = ARP(pdst=target_ip)
     ether = Ether(dst="FF:FF:FF:FF:FF:FF")
@@ -18,18 +17,5 @@ def arp_scan():
     for client in clients:
         print("{:16}    {}".format(client["IP"], client["MAC"]))
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("target", help="Network IP address")
-
-    args = parser.parse_args()
-
-    target_ip = args.target
-
-    print("\n" + "-"*45)
-    print("Performing ARP Scan on: " + target_ip)
-    print("Time started: " + str(datetime.now()))
-    print("-"*45 + "\n")
-
-    arp_scan()
+def end():
+    sys.exit()
--- Networking Tools/ARPTool/CLI/arpspoof.py
+++ Networking Tools/ARPTool/CLI/arpspoof.py
@@ -57,39 +57,3 @@ def restore(target_ip, host_ip, verbose=True):
         self_mac = ARP().hwsrc
         print("[+] Sent to {}: {} is at {}".format(target_ip, host_ip, self_mac))
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", help="Target IP address")
-    parser.add_argument("-g", "--gateway", help="Gateway IP address")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Default: True")
-
-    args = parser.parse_args()
-
-    target = args.target
-    host = args.gateway
-    verbose = args.verbose
-
-    verbose=True
-
-    print("\n" + "-"*45)
-    print("Performing ARP Spoofing attack on: " + target)
-    print("Time started: " + str(datetime.now()))
-    print("-"*45 + "\n")
-
-    enable_ip_route()
-
-    try:
-        while True:
-            arp_spoof(target, host, verbose)
-            arp_spoof(host, target, verbose)
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        print("\n[!] Restoring the network... Please wait.")
-        restore(target, host)
-        restore(host, target)
-        disable_ip_route()
-
-
-
